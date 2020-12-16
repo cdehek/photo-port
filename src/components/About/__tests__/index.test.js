82f6c58dd7e0507095ef9f6423c02aad6323ff3e
christian.dehek <christian.dehek@gmail.com>
2020-12-16 13:20:49 -0700

    updated codebase

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,20 +1,19 @@
-import React from "react";
-import { render, cleanup } from "@testing-library/react";
+// __tests__/About.test.js
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import About from "..";
+import About from '..';
 
 afterEach(cleanup);
 
-describe("About component", () => {
-  //first test
-  it("renders", () => {
+describe('About component', () => {
+  it('renders', () => {
     render(<About />);
   });
-
-  //second test
+  
   it('matches snapshot DOM node structure', () => {
-      //render about
-      const {asFragment} = render(<About />);
-      expect(asFragment()).toMatchSnapshot();
-  })
-});
+    const { asFragment } = render(<About />);
+    
+    expect(asFragment()).toMatchSnapshot();
+  });
+})
