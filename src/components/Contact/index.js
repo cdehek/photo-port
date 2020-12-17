d2bed0c37f8a6b38c1f469fb928331b8c91ebdd9
Christian <christian.dehek@gmail.com>
2020-12-17 13:20:36 -0700

    added contact form and necessary tests for it/passes all tests

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { validateEmail } from "../../utils/helpers";
 
 function ContactForm() {
   const [formState, setFormState] = useState({
@@ -6,27 +7,46 @@ function ContactForm() {
     email: "",
     message: "",
   });
+  const [errorMessage, setErrorMessage ] = useState('');
   const { name, email, message } = formState;
 
   function handleChange(e) {
+    if (e.target.name === "email") {
+      const isValid = validateEmail(e.target.value);
+      console.log(isValid);
+      // isValid conditional statement
+      if (!isValid) {
+          setErrorMessage('Your email is invalid');
+      } else {
+          setErrorMessage('');
+      }
+    } else {
+        if(!e.target.value.length) {
+            setErrorMessage(`${e.target.name} is required.`);
+        } else {
+            setErrorMessage('');
+        }
+    }if (!errorMessage) {
+        setFormState({ ...formState, [e.target.name]: e.target.vaule });
+    }
     setFormState({ ...formState, [e.target.name]: e.target.value });
   }
 
   function handleSubmit(e) {
-      e.preventDefault();
-      console.log(formState);
+    e.preventDefault();
+    console.log(formState);
   }
 
   return (
     <section>
-      <h1>Contact Me</h1>
+      <h1 data-testid="h1">Contact Me</h1>
       <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
           <input
             type="text"
             name="name"
-            onChange={handleChange}
+            onBlur={handleChange}
             defaultValue={name}
           />
         </div>
@@ -35,7 +55,7 @@ function ContactForm() {
           <input
             type="email"
             name="email"
-            onChange={handleChange}
+            onBlur={handleChange}
             defaultValue={email}
           />
         </div>
@@ -44,11 +64,16 @@ function ContactForm() {
           <textarea
             name="message"
             rows="5"
-            onChange={handleChange}
+            onBlur={handleChange}
             defaultValue={message}
           />
+          {errorMessage && (
+              <div>
+                  <p className="error-text">{errorMessage}</p>
+              </div>
+          )}
         </div>
-        <button type="submit">Submit</button>
+        <button data-testid="submit" type="submit">Submit</button>
       </form>
     </section>
   );
