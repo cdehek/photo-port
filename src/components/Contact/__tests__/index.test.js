e973ec7c1e4e9066062b4a050f953a2764b1f73b
christian.dehek <christian.dehek@gmail.com>
2020-12-18 13:02:11 -0700

    updated codebase/ also added modal for each image in gallery

--- src/components/Contact/__tests__/index.test.js
+++ src/components/Contact/__tests__/index.test.js
@@ -1,34 +1,29 @@
-import React from "react";
-import { render, cleanup } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import Contact from "..";
+// __tests__/Contact.test.js
+import React from 'react'
+import { render, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import Contact from './Contact'
 
-afterEach(cleanup);
+afterEach(cleanup)
 
-describe("Contact component", () => {
-  it("renders", () => {
-    render(<Contact />);
-  });
-
-  it("matches snapshot", () => {
-    const { asFragment } = render(<Contact />);
-
-    expect(asFragment()).toMatchSnapshot();
-  });
+describe('Contact component renders', () => {
+  it('renders', () => {
+  render(<Contact />);
 });
 
-describe("Contact me is visible", () => {
-  it("inserts text into <h1>", () => {
-    const { getByTestId } = render(<Contact />);
+  it('renders', () => {
+    const { asFragment } = render(<Contact />)
+    expect(asFragment()).toMatchSnapshot()
+  })
+})
 
-    expect(getByTestId("h1")).toHaveTextContent("Contact Me");
-  });
-});
 
-describe("Submit is visible", () => {
-    it("inserts text into button", () => {
-      const { getByTestId } = render(<Contact />);
-  
-      expect(getByTestId("submit")).toHaveTextContent("Submit");
-    });
-  });
+it('renders', () => {
+  const { getByTestId } = render(<Contact />)
+  expect(getByTestId('h1tag')).toHaveTextContent('Contact me')
+})
+ 
+it('renders', () => {
+  const { getByTestId } = render(<Contact />)
+  expect(getByTestId('button')).toHaveTextContent('Submit')
+})
